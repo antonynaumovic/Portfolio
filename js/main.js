23c4ca0e1edcb46a7fa339dc6ba92f4d0098cf22
Antony Naumovic <antonynaumovic@gmail.com>
2019-04-18 20:47:41 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -5,48 +5,116 @@ var lFollowX = 0,
     friction = 1 / 80;
 
 var item = document.querySelector(":root");
+var effect = $(".squareOutline");
+var cover = $(".cover");
+var progressBar = $(".progressBar");
+var clicked = false;
+var widthBar = 0;
 
-var multiplier = 1;
-
+var down = false;
+progressBar.addClass("progress-pause")
 
 $("body").mousedown(function() {
     console.log("yeet");
-    multiplier = 1.01;
+    down = true;
+
+    console.log(down);
+
+    if(down){
+      progressBar.removeClass("progress-pause")
+      progressBar.addClass("progress-running")
+    
+    }
+    else{
+      progressBar.removeClass("progress-running")
+      progressBar.addClass("progress-pause")
+    
+    }
   }
 ).mouseup(function() {
-    multiplier = 1;
-    
+  down = false;  
+  console.log(down);
+  if(down){
+    progressBar.removeClass("progress-pause")
+    progressBar.addClass("progress-running")
+  
+  }
+  else{
+    progressBar.removeClass("progress-running")
+    progressBar.addClass("progress-pause")
+  
+  }
+
 });
 
 
+
+
+function redirect(){
+  timeout2 = setTimeout( function() {
+  window.location.href = "/work.html"}
+  , 2000);
+}
+
+function scaleCover(){
+  cover.css({    
+  '-webit-transform': "scale(0)",
+  '-moz-transform': "scale(0)",
+  'transform': "scale(0)"
+})
+}
+
+function scaleCoverBig(){
+  cover.css({    
+  '-webit-transform': "scale(100)",
+  '-moz-transform': "scale(100)",
+  'transform': "scale(100)"
+})
+}
 
 
 function moveBackground() {
   x += (lFollowX - x) * friction;
   y += (lFollowY - y) * friction;
   
-  translate = 'translate(' + x + 'px, ' + y + 'px) scale('+ 1.1*multiplier +')';
+  translate = 'translate(' + x*2 + 'px, ' + y*2 + 'px) scale('+ 1.1 +')';
 
-  translate2 = 'translate(' + (-x) + 'px, ' + (-y) + 'px) scale('+ 1.2*multiplier +')';
+  translate2 = 'translate(' + (-x*2) + 'px, ' + (-y*2) + 'px) scale('+ 1.2 +')';
 
-  translate3 = 'translate(' + x*5 + 'px, ' + y*5 + 'px) scale('+ 1.1*multiplier +') rotate(45deg)';
+  translate3 = 'translate(' + x*5 + 'px, ' + y*5 + 'px) scale('+ 1.1 +') rotate(45deg)';
 
-  translate4 = 'translate(' + x*0.5 + 'px, ' + y*0.5 + 'px) scale('+ 1.1*multiplier +') rotate(45deg)';
+  translate4 = 'translate(' + x*-1 + 'px, ' + y*-1 + 'px) scale('+ 1 +') rotate(45deg)';
 
-  translate5 = 'translate(' + x*10 + 'px, ' + y*10 + 'px) scale('+ 1.1*multiplier +') rotate(45deg)';
+  translate5 = 'translate(' + x*10 + 'px, ' + y*10 + 'px) scale('+ 1.1 +') rotate(45deg)';
 
-  translate7 = 'translate(' + x*13 + 'px, ' + y*13 + 'px) scale('+ 1.1*multiplier +') rotate(45deg)';
+  translate7 = 'translate(' + x*13 + 'px, ' + y*13 + 'px) scale('+ 1.1 +') rotate(45deg)';
 
-  translate6 = 'translate(' + x*20 + 'px, ' + y*20 + 'px) scale('+ 1.1*multiplier +') rotate(45deg)';
+  translate6 = 'translate(' + x*20 + 'px, ' + y*20 + 'px) scale('+ 1.1 +') rotate(45deg)';
 
-  translate8 = 'translate(' + x*0.8 + 'px, ' + y*0.8 + 'px) scale('+ 1.1*multiplier +') ';
+  translate8 = 'translate(' + x*0.8 + 'px, ' + y*0.8 + 'px) scale('+ 1.1 +') ';
 
+  translate9 = 'translate(' + x*30 + 'px, ' + y*30 + 'px) scale('+ 1.1 +') ';
 
+  translate10 = 'translate(' + x*1.5 + 'px, ' + y*1.5 + 'px) scale('+ 1.1 +') ';
+
+  widthBar = progressBar.width();
+  widthBar = widthBar/ progressBar.parent().width() * 100;
+  console.log(widthBar);
+  if (widthBar >= 80){
+    scaleCoverBig();
+    redirect();
+  }
 
   $('.squareLines').css({
     '-webit-transform': translate8,
     '-moz-transform': translate8,
     'transform': translate8
+  });
+
+  $('.biggerHolder').css({
+    '-webit-transform': translate9,
+    '-moz-transform': translate9,
+    'transform': translate9
   });
 
   $('.squareBig').css({
@@ -71,6 +139,11 @@ function moveBackground() {
     '-webit-transform': translate4,
     '-moz-transform': translate4,
     'transform': translate4
+  });
+  $('.circle3').css({
+    '-webit-transform': translate10,
+    '-moz-transform': translate10,
+    'transform': translate10
   });
   $('.circle4').css({
     '-webit-transform': translate5,
@@ -114,5 +187,5 @@ $(window).on('mousemove click', function(e) {
   lFollowY = (10 * lMouseY) / 100;
 
 });
-
+scaleCover();
 moveBackground();
